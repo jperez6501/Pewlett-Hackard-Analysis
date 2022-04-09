a167368966c4a66ca525609a27a5b095ddcbf908
Jessica Perez <jessicaperez@jessicas-air.myfiosgateway.com>
2022-04-09 14:35:46 -0400

    adding for challange 7

--- Employee_Database_challenge.sql/Employee_Database_challenge2.sql
+++ Employee_Database_challenge.sql/Employee_Database_challenge2.sql
@@ -51,3 +51,30 @@ ON (de.emp_no = ti.emp_no)
 WHERE (birth_date BETWEEN '1965-01-01' AND '1965-12-31') AND de.to_date= ('9999-01-01')
 ORDER BY emp_no;
 
+--Deliverable 3
+--Finding the total count of employees getting ready to retire
+SELECT SUM(count)Total_Retiring_Employees
+INTO total_retiring_employees
+FROM retiring_titles
+
+SELECT * FROM total_retiring_employees;
+
+---Finding the total count of employees eligible for the mentorship program
+-- Employee count by department number
+SELECT COUNT(emp_no) AS count, title 
+INTO mentorship_count
+FROM mentorship_eligibility
+GROUP BY title
+ORDER BY count desc;
+SELECT SUM(count)total_mentorship
+INTO total_mentorship
+FROM mentorship_count
+
+-- Employees Membership count by department number
+SELECT COUNT(emp_no) AS count, title 
+INTO membership_eligibility_by_dept
+FROM mentorship_eligibility
+GROUP BY title
+ORDER BY count desc;
+
+SELECT * FROM retiring_titles;
